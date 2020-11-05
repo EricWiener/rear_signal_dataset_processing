ca2f6dc163791f34efa06f74fd1b360eb4eba8ed
Eric Wiener <ericwiener3@gmail.com>
2020-11-05 11:30:01 -0500

    FEAT: added get_paths_for_image_sequences to utils.py
    This allowed the function to be used by both processing for slowfast
    and for the lstm

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import shutil
 import numpy as np
 import imghdr  # for checking images are valid
 from shutil import rmtree
+import random
+from collections import defaultdict
 
 def get_immediate_directories(directory_path):
     """
@@ -83,3 +85,74 @@ def rm_mkdir(directory_name):
         # Remove the existing folder to get rid of old contents
         rmtree(directory_name)
     os.mkdir(directory_name)
+
+
+def get_paths_for_image_sequences(difficulties):
+    """
+    Gets the path for the images per sequence
+
+    - difficulties: an instance of DifficultyLevels (difficulty_level.py)
+    
+    Will return a dictionary per_class_image_sequences of form:
+        {
+            <label>: {
+                <sequence>: {
+                    difficulty: 'E', // 'E', 'M', 'H'
+                    image_paths: [...],
+                    split: "train", // "train", "test", "val"
+                },
+                <sequence>: {
+                    difficulty: 'E', // 'E', 'M', 'H'
+                    image_paths: [...],
+                    split: "val",
+                },
+            },
+            <label>: {
+                // ...
+            },
+        }
+    """
+    # Ensure the results are reproducable
+    random.seed(0)
+
+    per_class_image_sequences = defaultdict(lambda: defaultdict(dict))
+
+    # Footage directories
+    footage_dirs = get_immediate_directories('./data')
+
+    for f_dir in footage_dirs:
+        # These are folders corresponding to each class
+        path_1 = os.path.join('./data', f_dir)
+        f_class_dirs = get_immediate_directories(path_1)
+
+        # Loop through all sequence dirs of form Footage_name_XXX_DDD
+        # ex. 20160805_g1k17-08-05-2016_15-57-59_idx99_BOO
+        for f_class_dir in f_class_dirs:
+            # The label of the class ex. "BLO"
+            class_label, _ = get_label_sequence_from_name(f_class_dir)
+
+            path_2 = os.path.join(path_1, f_class_dir)
+            footage_sequence_dirs = get_immediate_directories(path_2)
+
+            # Loop through the individual sequences
+            # footage_sequence_dir can be of form 20160805_g1k17-08-05-2016_15-57-59_idx99_BOO_00023411
+            for footage_sequence_dir in footage_sequence_dirs:
+                path_3 = os.path.join(
+                    path_2, footage_sequence_dir, "light_mask")
+
+                difficulty_level = difficulties.get_difficulty_level(
+                    footage_sequence_dir)
+
+                # Save the difficulty level
+                per_class_image_sequences[class_label][footage_sequence_dir]["difficulty"] = difficulty_level
+                per_class_image_sequences[class_label][footage_sequence_dir]["split"] = get_split(
+                )
+
+                # Get the images
+                image_names = get_immediate_images(path_3)
+                image_names = [os.path.join(path_3, i) for i in image_names]
+
+                # Save all the valid image paths
+                per_class_image_sequences[class_label][footage_sequence_dir]["image_paths"] = image_names
+
+    return per_class_image_sequences
